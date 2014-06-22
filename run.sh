020b5798b4d002d6c25e3f2d044b32e7f640c0fb
Ron Cohen <ron@opbeat.com>
2014-06-22 12:00:00 +0200

    Updated to just use the env vars that wercker defines.

--- run.sh
+++ run.sh
@@ -1,15 +1,23 @@
-if [ ! -n "$WERCKER_CAMPFIRE_NOTIFY_TOKEN" ]; then
-  error 'Please specify token property'
-  exit 1
+#!/bin/sh
+set -e
+
+if [ ! -n "$WERCKER_OPBEAT_NOTIFY_ORG_ID" ]
+then
+    fail 'missing or empty option org_id, please check wercker.yml'
 fi
 
-OPBEAT_ORG_ID=`get_option opbeat_org_id`
-OPBEAT_APP_ID=`get_option opbeat_app_id`
-OPBEAT_SECRET_TOKEN=`get_option opbeat_secret_token`
+if [ ! -n "$WERCKER_OPBEAT_NOTIFY_APP_ID" ]
+then
+    fail 'missing or empty option app_id, please check wercker.yml'
+fi
 
+if [ ! -n "$WERCKER_OPBEAT_NOTIFY_SECRET_TOKEN" ]
+then
+    fail 'missing or empty option secret_token, please check wercker.yml'
+fi
 
-curl https://opbeat.com/api/v1/organizations/$OPBEAT_ORG_ID/apps/$OPBEAT_APP_ID/releases \
-    -H "Authorization: Bearer $OPBEAT_SECRET_TOKEN" \
+curl https://opbeat.com/api/v1/organizations/$WERCKER_OPBEAT_NOTIFY_ORG_ID/apps/$WERCKER_OPBEAT_NOTIFY_APP_ID/releases \
+    -H "Authorization: Bearer $WERCKER_OPBEAT_NOTIFY_SECRET_TOKEN" \
     -d rev=$WERCKER_GIT_COMMIT \
     -d branch=$WERCKER_GIT_BRANCH \
     -d status=completed
