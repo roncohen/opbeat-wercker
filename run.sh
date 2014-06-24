9d5301d7ac5876a597eb8f4d1f8a2b1eac888d71
Ron Cohen <ron@opbeat.com>
2014-06-24 09:56:57 +0200

    Opbeat return 202 on success.

--- run.sh
+++ run.sh
@@ -24,6 +24,15 @@ then
     fail 'missing or empty option secret_token, please check wercker.yml'
 fi
 
+echo curl https://opbeat.com/api/v1/organizations/$WERCKER_OPBEAT_NOTIFY_ORG_ID/apps/$WERCKER_OPBEAT_NOTIFY_APP_ID/releases/ \
+    -H "Authorization: Bearer $WERCKER_OPBEAT_NOTIFY_SECRET_TOKEN" \
+    -d rev=$WERCKER_GIT_COMMIT \
+    -d branch=$WERCKER_GIT_BRANCH \
+    -d status=completed \
+    --silent \
+    --write-out "%{http_code}" \
+    --output /dev/stderr
+
 result=`curl https://opbeat.com/api/v1/organizations/$WERCKER_OPBEAT_NOTIFY_ORG_ID/apps/$WERCKER_OPBEAT_NOTIFY_APP_ID/releases/ \
     -H "Authorization: Bearer $WERCKER_OPBEAT_NOTIFY_SECRET_TOKEN" \
     -d rev=$WERCKER_GIT_COMMIT \
@@ -34,7 +43,7 @@ result=`curl https://opbeat.com/api/v1/organizations/$WERCKER_OPBEAT_NOTIFY_ORG_
     --output /dev/stderr`
 
 
-if [ "$result" = "200" ]; then
+if [ "$result" = "202" ]; then
   success "Notified Opbeat"
 else
   fail "Failed to notify Opbeat"
